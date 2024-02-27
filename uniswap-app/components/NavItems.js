7fa2be6561cfc45b570e75cc9787f1d365eac6d2
KramesDt <lordkratos365@gmail.com>
2024-02-27 14:09:19 +0100

    feature: binanca api

--- uniswap-app/components/NavItems.js
+++ uniswap-app/components/NavItems.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
-import { ArrowSmUpIcon } from '@heroicons/react/outline'
+import React, { useState } from "react";
+import { ArrowSmUpIcon } from "@heroicons/react/outline";
 
 const NavItems = () => {
-  const SWAP = 'Swap'
-  const POOL = 'Pool'
-  const VOTE = 'Vote'
-  const CHART = 'Charts'
+  const SWAP = "Swap";
+  const TRADINGVIEW = "Trading View";
+  const VOTE = "Vote";
+  const CHART = "Charts";
 
-  const [selectedNavItem, setSelectedNavItem] = useState(SWAP)
-
+  const [selectedNavItem, setSelectedNavItem] = useState(SWAP);
+  const currentSelection = selectedNavItem;
+  if (currentSelection == "Swap") console.log("Lets swap");
+  console.log(currentSelection);
   return (
-    <div className='bg-zinc-900 h-fit flex items-center justify-around rounded-full mx-6'>
+    <div className="bg-zinc-900 h-fit flex items-center justify-around rounded-full mx-6">
       <p
         className={getNavIconClassName(SWAP)}
         onClick={() => setSelectedNavItem(SWAP)}
@@ -18,10 +20,10 @@ const NavItems = () => {
         {SWAP}
       </p>
       <p
-        className={getNavIconClassName(POOL)}
-        onClick={() => setSelectedNavItem(POOL)}
+        className={getNavIconClassName(TRADINGVIEW)}
+        onClick={() => setSelectedNavItem(TRADINGVIEW)}
       >
-        {POOL}
+        {TRADINGVIEW}
       </p>
       <p
         className={getNavIconClassName(VOTE)}
@@ -31,23 +33,23 @@ const NavItems = () => {
       </p>
       <p
         className={getNavIconClassName(CHART)}
-        onClick={() => window.open('https://info.uniswap.org/#/', '_blank')}
+        onClick={() => window.open("https://info.uniswap.org/#/", "_blank")}
       >
         {CHART}
-        <ArrowSmUpIcon className='h-4 rotate-45' />
+        <ArrowSmUpIcon className="h-4 rotate-45" />
       </p>
     </div>
-  )
+  );
 
   function getNavIconClassName(name) {
     let className =
-      'p-1 px-4 cursor-pointer border-[4px] border-transparent flex items-center'
+      "p-1 px-4 cursor-pointer border-[4px] border-transparent flex items-center";
     className +=
       name === selectedNavItem
-        ? ' bg-zinc-800 border-zinc-900 rounded-full'
-        : ''
-    return className
+        ? " bg-zinc-800 border-zinc-900 rounded-full"
+        : "";
+    return className;
   }
-}
+};
 
-export default NavItems
+export default { NavItems, currentSelection };
--- uniswap-app/pages/index.js
+++ uniswap-app/pages/index.js
@@ -1,11 +1,23 @@
 import Header from '../components/Header'
 import SwapComponent from '../components/SwapComponent'
+import TradingViewWidget from "../components/TradingView";
+import {NavItems, currentSelection } from "../components/NavItems";
+
 
 export default function Home() {
+  console.log("selectedNavItem is:",currentSelection);
+  const Swapper = ()=>{
+       if (selectedNavItem == "Trading View") {
+         return <TradingViewWidget />;
+       } else return <SwapComponent />;
+       
+
+  }
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-center bg-[#2D242F]'>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-[#2D242F]">
       <Header />
-      <SwapComponent />
+      <Swapper />
+      {/* <SwapComponent /> */}
     </div>
-  )
+  );
 }
